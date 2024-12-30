3f0f9518ccf1c395770e32b01be61095154d4d6a
codechakap <ncnkode@gmail.com>
2024-12-30 23:02:54 +0700

    Tes pertama

--- src/components/kontak.tsx
+++ src/components/kontak.tsx
@@ -1,1 +1,47 @@
-random lagi
+"use client";
+
+export default function VisiMisi() {
+    return (
+        <div className="leading-tight py-8">
+            <section className="scroll-mt-24" id="visi-misi">
+                <div className="max-w-7xl mx-auto px-4">
+                    <h1 className="text-3xl lg:text-6xl font-bold text-center mb-6 text-white">
+                        Apa yang Ingin Dibawakan Kamal?
+                    </h1>
+                    <p className="text-sm lg:text-lg text-center px-4 mb-6 text-white">
+                        gyatt rizz only in ohio duke dennis did you pray today livvy
+                        dunne rizzing up baby gronk sussy imposter pibby glitch in real life
+                        sigma alpha omega male grindset andrew tate goon cave freddy fazbear
+                        colleen ballinger smurf cat vs strawberry elephant blud dawg shmlawg
+                        ishowspeed a whole bunch of turbulence flabbergasted after that...
+                        cantt even mew in oklahoma free robux gltich working 2025 we can go
+                        gyatt for gyatt is that a jojo reference ? oh my god wat da hail!!!!
+                    </p>
+                </div>
+
+                <div className="mx-auto px-4 pt-1 max-w-7xl">
+                    <div className="text-gray-100 text-justify text-sm lg:text-lg bg-[#0053CF] rounded-3xl p-6 mb-4">
+                        <h2 className="text-lg font-bold text-center">Visi</h2>
+                    </div>
+                </div>
+
+                <div className="mx-auto px-4 pt-1 max-w-7xl">
+                    <div className="text-gray-100 text-justify text-sm lg:text-lg bg-[#0053CF] rounded-3xl p-6 mb-4">
+                        <h2 className="text-lg font-bold text-center">Misi</h2>
+                    </div>
+                </div>
+            </section>
+
+            <section className="scroll-mt-24" id="draft">
+                <div className="mx-auto px-4 pt-1 max-w-7xl">
+                    <div className="text-gray-100 text-justify text-sm lg:text-lg bg-[#0053CF] rounded-3xl p-6 mb-4">
+                        <h2 className="text-lg font-bold text-center">
+                            Draft Muhammad Kamal
+                        </h2>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+
