013a80153618011319f9f4e58bfb0d376dff239c
Alivican <Azwaaliyahz@gmail.com>
2025-01-22 15:48:25 +0700

    asdfkjlh???file conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,20 @@ import SurveyForm from "@/components/survey-form";
 
 export default function Page() {
   return (
-    <div className="min-h-screen bg-gradient-to-bl from-[#182c60] to-[#001524] text-white">
-      <div className="container mx-auto px-10">
-        <LandingHero />
-        <SurveyForm />
-      </div>
+    <div className="min-h-screen bg-[#181720] text-white">
+      <NavBar />
+      <main className="pt-24">
+        <section className="scroll-m-20" id="landing-hero">
+          <LandingHero />
+        </section>
+        <section className="scroll-m-20" id="tentang-kamal">
+          <TentangKamal />
+        </section>
+        <VisiMisi />
+        <section className="scroll-m-24" id="survey-form">
+          <SurveyForm />
+        </section>
+      </main>
     </div>
   );
 }
